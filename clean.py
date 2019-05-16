7ad9934fff0b940af8653d9c10964c188b765bcf
Brojowski <alex.gajowski@gmail.com>
2019-05-16 01:02:31 -0400

    Clean personal info out of CSV

--- clean.py
+++ clean.py
@@ -2,16 +2,47 @@
 # Convert JSON to CSV
 import util
 
-jsonDumps = [
-    "./json/khe16/users.json",
-    "./json/khe16/apps.json",
-    "./json/khe17/users.json",
-    "./json/khe17/apps.json",
-    "./json/khe18/users.json",
-    "./json/khe18/apps.json",
+csvDumps = [
+    "./csv/khe16/users.csv",
+    "./csv/khe16/apps.csv",
+    "./csv/khe17/users.csv",
+    "./csv/khe17/apps.csv",
+    "./csv/khe18/users.csv",
+    "./csv/khe18/apps.csv",
 ]
 
-for table in jsonDumps:
-    csv = table.replace("json","csv")
-    print(table," to ", csv)
-    util.jsonToCsv(table, csv)
+for table in csvDumps:
+    json = table.replace("csv","json")
+    print(json," to ", table)
+    util.jsonToCsv(json, table)
+
+#%%
+import pandas as pd
+
+#%%
+# Remove sensitive data from applications
+
+# 16: _id,name,school,phone,shirt,demographic,first,year,age,gender,major,conduct,status,going,checked,door,dietary,probable,created,__v,travel
+# 17: _id,name,school,phone,shirt,demographic,first,year,age,gender,major,conduct,link,status,going,checked,door,dietary,probable,created,__v
+# 18: _id,name,school,phone,shirt,demographic,first,year,age,gender,major,conduct,travel,resume,link,status,going,checked,door,dietary,extra,probable,created,__v
+
+sensitiveCols = [
+    "name",
+    "phone",
+    "__v",
+    "resume",
+    "link",
+    "extra"
+]
+
+for csv in filter(lambda x: "apps.csv" in x, csvDumps):
+    appDf = pd.read_csv(csv)
+
+    for col in sensitiveCols:
+        if col in appDf.columns:
+            appDf = appDf.drop(col, axis=1)
+    
+    appDf.to_csv(csv.replace("csv", "cleaned", 1), index=False)
+    
+
+#%%
